06cdded5deedb6f2884a0de11504fc6d87649da6
farhod <example@gmail.com>
2022-03-15 14:03:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use scrtirct';
+
+let result = 82;
+console.log(result);
+
+var name = "KAlaY";
+console.log(name);
+
+const lego = 232;
+console.log(lego);
+
+const man = ['black', 'long', 'strong'];
+console.log(man[0]);
---
+++ script2.js
@@ -0,0 +1,6 @@
+const answers = [];
+//answers[0] = prompt('What is your name?', '');
+//answers[1] = prompt('How old are you?', '');
+//answers[2] = prompt('How are you?', '');
+
+console.log(typeof(answers));
