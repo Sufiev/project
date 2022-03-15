613cadde19197b20167ccce47263a7014234ab16
farhod <example@gmail.com>
2022-03-15 14:07:56 +0300

    second commit

--- script2.js
+++ script2.js
@@ -1,6 +1,11 @@
-const answers = [];
+"use strict" ;
+//const answers = [];
 //answers[0] = prompt('What is your name?', '');
 //answers[1] = prompt('How old are you?', '');
 //answers[2] = prompt('How are you?', '');
 
-console.log(typeof(answers));
+//console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`http://something.com/${category}/5`);
